afd20b967056c9f3a2182dcdb5967ca1601738ea
unknown <tharolakhila1@gmail.com>
2025-06-09 22:55:52 +0530

    app.js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
+require('dotenv').config(); // Load environment variables first
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const passport = require('./config/passport');
 const db = require('./config/db');
 const userRouter = require('./routes/userRouter');
 const adminRouter = require('./routes/adminRouter');
-const userMiddleware = require('./middlewares/userMiddleware'); 
-
-const passport = require('./config/passport');
-
 
 const app = express();
 
+// Connect to MongoDB
 db();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -24,22 +22,21 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: false, 
+        secure: false, // Set to true in production with HTTPS
         httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 72 
+        maxAge: 1000 * 60 * 60 * 72 // 72 hours
     }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.use((req, res, next) => {
     res.locals.user = req.session.user || null;
     next();
 });
 
-//setting ejs views
+// Set EJS views
 app.set('view engine', 'ejs');
 app.set('views', [
     path.join(__dirname, 'views'),
@@ -47,15 +44,17 @@ app.set('views', [
     path.join(__dirname, 'views/user')
 ]);
 
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/Uploads', express.static(path.join(__dirname, 'public/Uploads')));
 
+// Routes
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(` Server running at: http://localhost:${PORT}`);
+    console.log(`Server running at: http://localhost:${PORT}`);
 });
 
 module.exports = app;
--- config/passport.js
+++ config/passport.js
@@ -1,61 +1,46 @@
 const passport = require('passport');
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/userSchema');
-const env = require('dotenv').config();
-
 
 passport.use(new GoogleStrategy({
-    clientID:process.env.GOOGLE_CLIENT_ID,
-    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
-
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'http://localhost:4002/auth/google/callback' // Use absolute URL
 },
-
-
-    async(accessToken,refreshToken,profile,done)=>{
-        console.log("Google Profile", profile); 
-        try {
-            
-            let user = await User.findOne({googleId:profile.id});
-            if(user){
-                return done(null,user);
-            }else{
-                user = new User({
-                    name:profile.displayName,
-                    email:profile.emails[0].value,
-                    googleId:profile.id,
-                });
-                await user.save();
-                return done(null,user);
-            }
-
-        } catch (error) {
-            
-            return done(error,null)
-
+async (accessToken, refreshToken, profile, done) => {
+    console.log('Google Profile', profile); 
+    try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+            return done(null, user);
+        } else {
+            user = new User({
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                googleId: profile.id,
+            });
+            await user.save();
+            return done(null, user);
         }
+    } catch (error) {
+        return done(error, null);
     }
+}));
 
-))
-console.log("Client ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("Client Secret:", process.env.GOOGLE_CLIENT_SECRET);
+console.log('Client ID:', process.env.GOOGLE_CLIENT_ID);
+console.log('Client Secret:', process.env.GOOGLE_CLIENT_SECRET);
 
-
-
-passport.serializeUser((user,done)=>{
-
-    done(null,user.id)
-
+passport.serializeUser((user, done) => {
+    done(null, user.id);
 });
 
-passport.deserializeUser(async(id,done)=>{
-   try {
-    const user = await User.findById(id);
-    done (null,user)
-   } catch (error) {
-    done(err,null)
-   }
-})
-
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (error) {
+        done(error, null);
+    }
+});
 
 module.exports = passport;
